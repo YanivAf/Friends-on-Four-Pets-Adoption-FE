bd0c5c9603fef1e7faede109aa977e95652e6097
Yaniv Aflalo <yanivaflalo@gmail.com>
2021-12-30 21:17:19 +0200

    deployment attempt 3

--- src/utils/connection.ts
+++ src/utils/connection.ts
@@ -1,3 +1,5 @@
 export {};
 
-export const DOMAIN = "http://localhost:5000"
+const local: boolean = false;
+
+export const DOMAIN = local ? "http://localhost:5000" : "https://friends-on-four.herokuapp.com/"
